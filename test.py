fd661f7f84bd42328d191296c778c27c5907bc6c
pablcol <pablcol@gmail.com>
2021-11-12 13:48:40 +0100

    test commit

---
+++ test.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+# Initialize tkinter
+root = Tk()
+root.title("Pedidos Grant Esgrima: Equipaciones")
+
+# Create label texts
+equipLabel = Label(root, text="Equipación completa")
+
+# Define entries
+
+# Define buttons
+equipButton = Button(root, text="Equipación completa")
+
+# Put elements on app
+equipButton.grid(row=0, column=0)
+
+# Initialize main loop and app
+root.mainloop()
