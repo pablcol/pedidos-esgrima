33d83e296d2bcfcbfd2fa0fb30173a489bf35006
pablcol <pablcol@gmail.com>
2021-11-15 01:50:57 +0100

    added justify left on labels

--- medidas.py
+++ medidas.py
@@ -4,12 +4,16 @@ from tkinter import *
 root = Tk()
 root.title("Pedidos Grant Esgrima")
 
+# Define functions
+def medidas():
+    return
+
 # Create label texts
 inicioLabel = Label(root, text="Medidas tirador/a")
-alturaLabel = Label(root, text="Altura en cm")
-pechoLabel = Label(root, text="Tamaño pecho en cm")
-cinturaLabel = Label(root, text="Tamaño cintura en cm")
-caderaLabel = Label(root, text="Tamaño cadera en cm")
+alturaLabel = Label(root, text="Altura en cm:")
+pechoLabel = Label(root, text="Tamaño pecho en cm:")
+cinturaLabel = Label(root, text="Tamaño cintura en cm:")
+caderaLabel = Label(root, text="Tamaño cadera en cm:")
 
 # Define entries
 alturaEntry = Entry(root, width=15)
@@ -23,16 +27,16 @@ buttonCalcular = Button(root, text="Calcular talla")
 # Put elements on app
 inicioLabel.grid(row=0, column=0, columnspan=2)
 
-alturaLabel.grid(row=1, column=0)
+alturaLabel.grid(row=1, column=0, sticky=W)
 alturaEntry.grid(row=1, column=1)
 
-pechoLabel.grid(row=2, column=0)
+pechoLabel.grid(row=2, column=0, sticky=W)
 pechoEntry.grid(row=2, column=1)
 
-cinturaLabel.grid(row=3, column=0)
+cinturaLabel.grid(row=3, column=0, sticky=W)
 cinturaEntry.grid(row=3, column=1)
 
-caderaLabel.grid(row=4, column=0)
+caderaLabel.grid(row=4, column=0, sticky=W)
 caderaEntry.grid(row=4, column=1)
 
 buttonCalcular.grid(row=5, column=1)
